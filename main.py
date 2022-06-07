d7b44b7eb5d29f0081c3c9dc896767efd52b2d78
MDHABIBURRAHMANHABIB <101259501+MDHABIBURRAHMANHABIB@users.noreply.github.com>
2022-06-07 11:15:01 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+'''Question 01: Write a program that switches the values stored in the variables a and b.
+Your program should work for different inputs. e.g. any value of a and b.'''
+
+a = input("a: ")
+b = input("b: ")
+print("a: " + a)
+print("b: " + b)
+
+'''Question 02: There are errors in all of the lines of code. Fix the code so that it runs without errors. The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise, the tests
+won't pass.
+
+When you run your program, it should print the following:
+#Your Output should be exactly like the below 👇
+
+5Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+#Your Output should be exactly like the above 👆
+
+#Fix the code below 👇'''
+
+print("Day 1-String Manipulation")
+print("String Concatenation is done with the ‘+’ sign")
+print('Hello' + 'world')
+print("New lines can be created with a backslash and n.")
